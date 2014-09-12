062d4f3430ab9a043e6ca3b2efb09c9bd4ca62a8
tejas.swastika@gmail.com <tejas.swastika@gmail.com>
2014-09-12 16:07:21 +0530

    added files
    added the required files to the master branch , before starting the
    project

---
+++ app/src/main/java/com/example/tejasad/iot/MyActivity.java
@@ -0,0 +1,62 @@
+package com.example.tejasad.iot;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+
+
+public class MyActivity extends Activity {
+    private static int RESULT_LOAD_IMAGE = 1;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my);
+
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.my, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void btnGalLoadClick(View view) {
+
+        Button btnLoadClick = (Button) view;
+        Intent mediaIntent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        startActivityForResult(mediaIntent,RESULT_LOAD_IMAGE);
+
+
+    }
+
+    protected void onActivityResult(int requestCode, int resultCode, Intent data)
+    {
+        if(requestCode== RESULT_LOAD_IMAGE && resultCode == RESULT_OK && data !=null )
+        {
+
+
+        }
+
+
+
+    }
+}
